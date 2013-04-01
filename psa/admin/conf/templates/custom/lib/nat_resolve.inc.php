08468d81c2b2551bc23b7dc8875d3fd37ff0ba0b
Wm. Li <williamli@pixls.co>
2013-04-01 16:58:15 +0800

    updated the NAT resolver to only support the most basic level of ipaddresses

--- psa/admin/conf/templates/custom/lib/nat_resolve.inc.php
+++ psa/admin/conf/templates/custom/lib/nat_resolve.inc.php
@@ -9,12 +9,19 @@ function nat_resolve($ipAddress)
 			"54.251.139.29" => "10.0.0.205",
 			"54.251.145.221" => "10.0.0.207",
 		);
+	/*
+
 	$ipAddress->escapedPublicAddress = $ipAddress->escapedAddress;
 
 	if (isset($nat_translation[$ipAddress->escapedAddress]))
 		$ipAddress->escapedAddress = $nat_translation[$ipAddress->escapedAddress];
 
 	$ipAddress->escapedPrivateAddress = $ipAddress->escapedAddress;
+	*/
+
+	if (isset($nat_translation[$ipAddress]))
+		$ipAddress = $nat_translation[$ipAddress];
+
 }
 
 ?>
