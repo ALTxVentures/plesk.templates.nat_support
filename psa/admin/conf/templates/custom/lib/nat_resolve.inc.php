e1fe026febe2a23868916294111f750b9d83d691
Wm. Li <williamli@pixls.co>
2013-04-01 19:14:34 +0800

    wrapped nat resolver inside !function_exists wrapper

--- psa/admin/conf/templates/custom/lib/nat_resolve.inc.php
+++ psa/admin/conf/templates/custom/lib/nat_resolve.inc.php
@@ -1,29 +1,33 @@
 <?php
 
-function nat_resolve($ipAddress)
+if (!function_exists(nat_resolve))
 {
-	$nat_translation = array(
-			"54.251.139.50" => "10.0.0.206",
-			"54.251.139.25" => "10.0.0.108",
-			"54.251.139.90" => "10.0.0.204",
-			"54.251.139.29" => "10.0.0.205",
-			"54.251.145.221" => "10.0.0.207",
-		);
-	/*
+	function nat_resolve($ipAddress)
+	{
+		$nat_translation = array(
+				"54.251.139.50" => "10.0.0.206",
+				"54.251.139.25" => "10.0.0.108",
+				"54.251.139.90" => "10.0.0.204",
+				"54.251.139.29" => "10.0.0.205",
+				"54.251.145.221" => "10.0.0.207",
+			);
+		/*
 
-	$ipAddress->escapedPublicAddress = $ipAddress->escapedAddress;
+		$ipAddress->escapedPublicAddress = $ipAddress->escapedAddress;
 
-	if (isset($nat_translation[$ipAddress->escapedAddress]))
-		$ipAddress->escapedAddress = $nat_translation[$ipAddress->escapedAddress];
+		if (isset($nat_translation[$ipAddress->escapedAddress]))
+			$ipAddress->escapedAddress = $nat_translation[$ipAddress->escapedAddress];
 
-	$ipAddress->escapedPrivateAddress = $ipAddress->escapedAddress;
-	*/
+		$ipAddress->escapedPrivateAddress = $ipAddress->escapedAddress;
+		*/
 
-	if (isset($nat_translation[$ipAddress]))
-		$ipAddress = $nat_translation[$ipAddress];
+		if (isset($nat_translation[$ipAddress]))
+			$ipAddress = $nat_translation[$ipAddress];
 
-	return $ipAddress;
+		return $ipAddress;
 
+	}
 }
+
 
 ?>
